7b060a712faa351134b1ca57b8d50f1a69f1f677
Steven Wang <steven@luabase.com>
2025-09-11 18:15:21 -0400

    use - instead of . for stream name separators

--- tap_redshift/__init__.py
+++ tap_redshift/__init__.py
@@ -163,11 +163,11 @@ def discover_catalog(conn, db_name, db_schemas):
         db_name = conn.get_dsn_parameters()['dbname']
         metadata = create_column_metadata(
             db_name, cols, is_view, table_name, key_properties)
-        qualified_table_name = '{}.{}'.format(
+        qualified_table_name = '{}-{}'.format(
             table_spec_dict.get(table_name)['table_schema'],
             table_name,
         )
-        tap_stream_id = '{}.{}'.format(
+        tap_stream_id = '{}-{}'.format(
             db_name, qualified_table_name)
 
         entry = CatalogEntry(
@@ -339,7 +339,7 @@ def sync_table(connection, catalog_entry, state):
     tap_stream_id = catalog_entry.tap_stream_id
     LOGGER.info('Beginning sync for {} table'.format(tap_stream_id))
     with connection.cursor(f"redshift_cursor_{secrets.token_hex(8)}") as cursor:
-        schema, table = catalog_entry.table.split('.')
+        schema, table = catalog_entry.table.split('-')
         database = catalog_entry.database
         select = 'SELECT {} FROM {}.{}.{}'.format(
             ','.join('"{}"'.format(c) for c in columns),
